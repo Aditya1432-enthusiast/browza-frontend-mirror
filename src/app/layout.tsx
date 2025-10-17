c88810743abe8abf3634e7c4c265be44d7cf4f50
Aditya Jadhav <aditya.jadhav@browza.in>
2025-10-18 02:32:22 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StoreProvider> 
-            <Providers>
+        <StoreProvider>
 
-             {children}
-            </Providers>
-  
+             {children}  
           
           </StoreProvider>  
            <Toaster /> 
