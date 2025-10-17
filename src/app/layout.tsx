16c064ace0220dfdbc9e39f3866f67c41aff2e30
Aditya Jadhav <aditya.jadhav@browza.in>
2025-10-18 02:18:57 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
 import StoreProvider from "./storeProvider";
 
-const geistSans = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
+const geistSans = GeistSans({ variable: "--font-geist-sans" });
 
-const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
+const geistMono = GeistMono({ variable: "--font-geist-mono" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
